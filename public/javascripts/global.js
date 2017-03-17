1adf0c446573d3ccfd7c80588bf281dfb3e664be
yudielgc <yudielgc@gmail.com>
2017-03-16 23:02:27 -0400

    fixing google redirection issues

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -47,8 +47,8 @@
                 tableContent += '<tr>';
                 tableContent += '<th scope="row">' + this.uniform_number + '</th>';
                 tableContent +=
-                    '<td><a href="https://www.google.com/search?q=' + this.first_name + '+' +  + this.last_name + '" target="_blank">' +
-                    this.first_name + ' ' + this.last_name + '</a></td>';
+                    '<td><a href="https://www.google.com/search?q=' + this.display_name + '" target="_blank">' +
+                    this.display_name + '</a></td>';
                 tableContent += '<td>' + this.team + '</td>';
                 tableContent += '<td>' + this.birthdate + '</td>';
                 tableContent += '<td>' + this.height_in + '</td>';
@@ -97,8 +97,8 @@
             tableContent += '<tr>';
             tableContent += '<th scope="row">' + this.uniform_number + '</th>';
             tableContent +=
-                '<td><a href="https://www.google.com/search?q=' + this.first_name + '+' +  + this.last_name + '" target="_blank">' +
-                this.first_name + ' ' + this.last_name + '</a></td>';
+                '<td><a href="https://www.google.com/search?q=' + this.display_name + '" target="_blank">' +
+                this.display_name + '</a></td>';
             tableContent += '<td>' + this.team + '</td>';
             tableContent += '<td>' + this.birthdate + '</td>';
             tableContent += '<td>' + this.height_in + '</td>';
